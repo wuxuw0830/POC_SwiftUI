170382200277351a0c83701e14681ed41699e673
KellyWu <wuxuw0830@gmail.com>
2024-08-07 13:39:36 +0800

    Initial Commit

---
+++ app/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  app
+//
+//  Created by Reed on 2024/8/5.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ app/appApp.swift
@@ -0,0 +1,32 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Reed on 2024/8/5.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct appApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
